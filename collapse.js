eb39749e679c0834cbee609c532da9d25e7ffd33
fiserto1 <fiserto1@fel.cvut.cz>
2015-07-21 16:23:54 +0200

    pridani vysouvaciho panelu nastaveni

---
+++ collapse.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Tomas on 21. 7. 2015.
+ */
+$(document).ready(function(){
+    $("#settings").on("hide.bs.collapse", function(){
+        $("#settingsIcon").css("color", "black");
+    });
+    $("#settings").on("show.bs.collapse", function(){
+        $("#settingsIcon").css("color", "deepskyblue");
+    });
+});
