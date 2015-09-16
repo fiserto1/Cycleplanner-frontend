7468e0447667bebe848968d5a4cab0b8b48bca93
fiserto1 <fiserto1@fel.cvut.cz>
2015-09-16 19:23:51 +0200

    odebrano oznaceni polozky menu po ztraceni modalu pro feedback,about,API

--- sidebar.js
+++ sidebar.js
@@ -5,4 +5,13 @@ $(document).ready(function() {
     $("#menu-icon").click(function(e) {
         //$(".modal-sm").show("slide");
     });
+    $("#about-panel").on("hidden.bs.modal", function(e) {
+        focusout();
+    });
+    $("#feedback-panel").on("hidden.bs.modal", function(e) {
+        focusout();
+    });
+    $("#api-panel").on("hidden.bs.modal", function(e) {
+        focusout();
+    });
 });
